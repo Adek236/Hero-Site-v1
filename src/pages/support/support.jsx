02b981958beabb6ab22582f33390baa63205411f
Adek236 <92857848+Adek236@users.noreply.github.com>
2022-06-10 11:38:22 +0000

    Support page event handling added.

--- src/pages/support/support.jsx
+++ src/pages/support/support.jsx
@@ -1,7 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './support.css';
 
 function Support(props) {
+  const [inputs, setInputs] = useState({});
+
+  const validationCheck = (name, value) => {
+    
+  }
+  
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    // if (!validationCheck(name, value)) return console.log("zle");
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  }
   
 	useEffect(() => {
 	  props.setShowHeroBtns(false);
@@ -13,23 +31,44 @@ function Support(props) {
         <h1>Submit a request</h1>
         <h2>We're here to help you!</h2>
       </section>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>YOUR EMAIL ADRESS
-          <input type="email"></input>
+          <input 
+            type="email" 
+            name="email" 
+            value={inputs.email || ""} 
+            onChange={handleChange}
+            required
+          />
         </label>
         <label>CHOOSE A REQUEST TYPE
-           <select>
-             <option value="0">-</option>
-              <option value="A">A</option>
-              <option value="B">B</option>
-              <option value="C">C</option>
+           <select
+             name="reqType" 
+             value={inputs.reqType = "general" || ""} 
+             onChange={handleChange}
+           >
+              <option value="general">General Question</option>
+              <option value="report">Report A Player</option>
+              <option value="technical">Technical Problem</option>
+              <option value="recover">Recover My Account</option>
             </select>
         </label>
         <label>SUBJECT
-          <input type="text"></input>
+          <input 
+            type="text" 
+            name="subject" 
+            value={inputs.subject || ""} 
+            onChange={handleChange}
+            required 
+          />
         </label>
         <label>DESCRIPTION
-          <textarea></textarea>
+          <textarea 
+            name="desc" 
+            value={inputs.desc || ""} 
+            onChange={handleChange}
+            required
+          ></textarea>
         </label>
         <input type="submit" value="SUBMIT"/>
       </form>
